a3d2e66a3b316c2dfe5408cb19107149ad235586
Julia Ishibashi <juliaishibashi@uvic.ca>
2024-07-02 23:29:03 -0700

    add bubble_sort

--- sort.py
+++ sort.py
@@ -7,8 +7,13 @@ class DrawInfomation:
 	white = 255, 255, 255
 	green = 0, 255, 0
 	red = 255, 0, 0
-	gray = 128, 128, 128
 	background_color = white
+
+	gradients = [(128, 128, 128), (160, 160, 160), (192, 192, 192)]
+
+
+	font = pygame.font.SysFont('comicsans', 30)
+	large_font = pygame.font.SysFont('comicsans', 40)
 
 	side_pad = 100
 	top_pad = 100
@@ -32,6 +37,35 @@ class DrawInfomation:
 		self.block_height = round((self.height - self.top_pad) / (self.max_val - self.min_val))
 		self.start_x = self.side_pad // 2 
 
+def draw(draw_info):
+	draw_info.window.fill(draw_info.background_color)
+
+	controles = draw_info.font.render("Press 'r' to reset, 'space' to sort, 'a' to sort accending, 'd' to sort decending", 1, draw_info.black)
+	draw_info.window.blit(controles, (draw_info.width/2 - controles.get_width()/2, 5))
+
+	sorting = draw_info.font.render("Press 'i' for insertion sort, 'b' for bubble sort", 1, draw_info.black)
+	draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2, 35))
+
+	draw_list(draw_info)
+	pygame.display.update()
+
+def draw_list(draw_info, color_pos={}):
+	lst = draw_info.lst
+
+	for i, val in enumerate(lst):
+		x = draw_info.start_x + i * draw_info.block_width
+		y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+
+		color = draw_info.gradients[i % len(draw_info.gradients)] #every single gradient we reset. 
+
+		if i in color_pos:
+			color = draw_info.gradients[color_pos[i] % len(draw_info.gradients)]
+
+		pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height)) #how far down to draw the block
+
+# n is the number of elements in the list
+# min_val is the minimum value of the elements in the list
+# max_val is the maximum value of the elements in the list
 def generate_starting_list(n, min_val, max_val):
 	lst = []
 
@@ -39,6 +73,20 @@ def generate_starting_list(n, min_val, max_val):
 		val = random.randint(min_val, max_val)
 		lst.append(val)
 
+	return lst
+
+def bubble_sort(draw_info, ascending=True):
+	lst = draw_info.lst
+
+	for i in range(len(lst) - 1):
+		for j in range(len(lst) - i - 1):
+			num1 = lst[j]
+			num2 = lst[j+1]
+
+			if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
+				lst[j], lst[j+1] = lst[j+1], lst[j] #swap
+				#draw()
+				yield True #generate a value
 	return lst
 
 def main():
@@ -51,16 +99,36 @@ def main():
 
 	lst = generate_starting_list(n, min_val, max_val)
 	draw_info = DrawInfomation(800, 600, lst)
+	sorting = False
+
+	ascending = True
 
 	while run:
 		clock.tick(60)
-
-		pygame.display.update()
-
+		draw(draw_info)
 		for event in pygame.event.get():
-			if event == pygame.QUIT:
+			if event.type == pygame.QUIT:
 				run = False
-	pygame.quite()
+
+			if event.type != pygame.KEYDOWN:
+				continue
+
+			if event.key == pygame.K_r:
+				#reset the list
+				lst = generate_starting_list(n, min_val, max_val)
+				draw_info.set_list(lst)
+				sorting = False
+
+			elif event.key == pygame.K_SPACE and sorting == False:
+				#sort the list
+				sorting = True
+
+			elif event.key == pygame.K_a and sorting == False:
+				ascending = True
+			elif event.key == pygame.K_d and sorting == False:
+				ascending = False
+
+	pygame.quit()
 
 if __name__ == "__main__":
 	main()
